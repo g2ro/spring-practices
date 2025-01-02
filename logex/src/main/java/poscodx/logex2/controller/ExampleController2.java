4593a68da76062f7d13f20b4bc6659cb50ec038f
kilho <kilho@localhost>
2025-01-02 13:43:57 +0900

    slf4j #finish

--- logex/src/main/java/poscodx/logex2/controller/ExampleController2.java
+++ logex/src/main/java/poscodx/logex2/controller/ExampleController2.java
@@ -12,21 +12,21 @@ public class ExampleController2 {
 	/**
 	 *  Logger 생성
 	 */
-	private static final Log LOG = LogFactory.getLog( ExampleController2.class );
+	private static final Log Log = LogFactory.getLog( ExampleController2.class );
 	
 	
-	@RequestMapping( "/ex2" )
+	@RequestMapping("/ex2")
 	@ResponseBody
 	public String ex2() {
 		
 		/**
 		 *  이 예제에서는 logback.xml 에서,
 		 *  
-		 *  1. fileAppender2 와 "com.douzone.logexample2" logger 를 살펴 보아야 합니다.
+		 *  1. fileAppender2 와 "poscodx.logex2.logexample2" logger 를 살펴 보아야 합니다.
 		 *  
-		 *  2. fileAppender2 에서는 /logex/logex2.log 로그파일 지정과
+		 *  2. fileAppender2 에서는 /User/사용자명/logex/logex2.log 로그파일 지정과
 		 *  
-		 *  3. 50KB 가 넘었을 때, /logex/logex2.i.log.zip 으로 압축되고 새로 
+		 *  3. 50KB 가 넘었을 때, /User/사용자명/logex/logex2.i.log.zip 으로 압축되고 새로 
 		 *     logex2.log 파일이 생성되는 것을 확인해 봐야 합니다.
 		 *    
 		 *  4. 새로운 로거를 세팅하고 있습니다.
@@ -36,10 +36,10 @@ public class ExampleController2 {
 		 *  5. 이 로거의 로그 레벨이 INFO 인 것도 로그 파일에서 확인해 보세요.   
 		 */
 
-		LOG.debug( "#ex2 - debug log" );
-		LOG.info( "#ex2 - info log" );
-		LOG.warn( "#ex2 - warn log" );
-		LOG.error( "#ex2 - error log" );
+		Log.debug( "#ex2 - debug log" );
+		Log.info( "#ex2 - info log" );
+		Log.warn( "#ex2 - warn log" );
+		Log.error( "#ex2 - error log" );
 		
 		return "Logback Logging Example2";
 	}
